679fa3d3aa497fde0b33f6b52b127690e2562c62
Peanuts-83 <tranque@free.fr>
2022-10-08 17:37:47 +0200

    connect service to module

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
   public users$: BehaviorSubject<string[]> = new BehaviorSubject(['Roger','Tango','Charlie'])
 
